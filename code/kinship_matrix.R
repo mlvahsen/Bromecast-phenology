907c5ba7b52401ae98b2b38ace48084c497a4d6a
mlvahsen <mlvahsen@gmail.com>
2023-07-27 16:58:39 -0400

    cleaning up code and adding preliminary graphics

--- code/kinship_matrix.R
+++ code/kinship_matrix.R
@@ -1,201 +1,114 @@
-## Load libraries ####
+# This code fits the linear model for flowering time
+
+# Load libraries
 library(tidyverse); library(mgcv); library(gratia); library(geomtextpath);
 library(here); library(readr); library(brms); library(RcppCNPy)
 
-## Source code for genotype climate of origin ####
-source(here("supp_code/climate_of_origin.R"))
-
-## Read in Sheep Station data ####
-
-# Read in derived phenology data
-phen_SS <- read_csv("~/Git/Bromecast/gardens/deriveddata/SS2022_growthphenology_with_harvest.csv")
-# Read in plant ID info
-ids_SS <- read_csv("~/Git/Bromecast/gardens/deriveddata/SS2022_plantID.csv")
-# Read in flagging data
-flags_SS <- read_csv("~/Git/Bromecast/gardens/deriveddata/SS2022_flags.csv")
-gardens <- read_csv("~/Git/Bromecast/gardens/rawdata/garden_treatments.csv")
-# Merge together datasets
-phen_id_SS <- merge(phen_SS, ids_SS)
-
-# Rename 'garden' column and remove cum_plot column
-gardens %>% 
-  mutate(site = garden) %>% 
-  dplyr::select(-cum_plot, -garden) -> gardens_sub
-
-# Merge together datasets
-phen_id_garden_SS <- merge(phen_id_SS, gardens_sub)
-
-# Merge together datasets
-phen_SS <- merge(phen_id_garden_SS, flags_SS)
-phen_SS <- merge(phen_SS, genotype_PCclimate)
-
-# Set appropriate factors for variables
-phen_SS %>% 
-  mutate(block = as.factor(block),
-         plot = as.factor(plot),
-         growout = as.factor(growout),
-         density = as.factor(density),
-         gravel = as.factor(gravel),
-         site = as.factor(site),
-         genotype = as.factor(genotype)) %>% 
-  mutate(plot_unique = as.factor(paste(site, block, plot, sep = "_")),
-         block_unique = as.factor(paste(site, block, sep = "_")))-> phen_SS
-
-
-## Read in Boise data ####
-# Read in derived phenology data
-phen_Boise <- read_csv("~/Git/Bromecast/gardens/deriveddata/Boise2022_growthphenology_by_plantID.csv")
-# Read in plant ID info
-ids_Boise <- read_csv("~/Git/Bromecast/gardens/deriveddata/Boise2022_plantID.csv")
-# Read in flagging data
-flags_Boise <- read_csv("~/Git/Bromecast/gardens/deriveddata/Boise2022_flags.csv")
-
-# Merge together datasets
-phen_id_Boise <- merge(phen_Boise, ids_Boise)
-
-# Merge together datasets
-phen_id_garden_Boise <- merge(phen_id_Boise, gardens_sub)
-
-# Merge together datasets
-phen_Boise <- merge(phen_id_garden_Boise, flags_Boise)
-phen_Boise <- merge(phen_Boise, genotype_PCclimate)
-
-# Set appropriate factors for variables
-phen_Boise %>% 
-  mutate(block = as.factor(block),
-         plot = as.factor(plot),
-         growout = NA,
-         density = as.factor(density),
-         gravel = as.factor(gravel),
-         site = as.factor(site),
-         genotype = as.factor(genotype)) %>% 
-  mutate(plot_unique = as.factor(paste(site, block, plot, sep = "_")),
-         block_unique = as.factor(paste(site, block, sep = "_")))-> phen_Boise
-
-
-## Read in Cheyenne data ####
-# Read in derived phenology data
-phen_CH <- read_csv("~/Git/Bromecast/gardens/deriveddata/CH2022_growthphenology_by_plantID.csv")
-# Read in plant ID info
-ids_CH <- read_csv("~/Git/Bromecast/gardens/deriveddata/CH2022_plantID.csv")
-# Read in flagging data
-flags_CH <- read_csv("~/Git/Bromecast/gardens/deriveddata/CH2022_flags.csv")
-
-# Merge together datasets
-phen_id_CH <- merge(phen_CH, ids_CH)
-
-# Merge together datasets
-phen_CH <- merge(phen_id_CH, flags_CH)
-phen_CH <- merge(phen_CH, genotype_PCclimate)
-
-# Set appropriate factors for variables
-phen_CH %>% 
-  mutate(block = as.factor(block),
-         plot = as.factor(plot),
-         growout = NA,
-         density = as.factor(case_when(density == "high" ~ "hi",
-                                       density == "low" ~ "lo")),
-         gravel = as.factor(gravel),
-         site = as.factor(site),
-         genotype = as.factor(genotype)) %>% 
-  mutate(plot_unique = as.factor(paste(site, block, plot, sep = "_")),
-         block_unique = as.factor(paste(site, block, sep = "_")))-> phen_CH
-
-## Merge datasets together ####
-phen <- rbind(phen_SS %>% dplyr::select(-tillers), phen_Boise, phen_CH)
-
-# Remove all other intermediate data sets
-rm(list=setdiff(ls(), "phen"))
-
-## Calculate time to first flower ####
-
-# Calculate when the earliest flowering day is for each plant. This procedure
-# removes plants that do not flower of the course of the experiment.
-phen %>%
-  filter(v %in% c("FG", "FP", "FB", "FX")) %>%
-  group_by(plantID) %>%
-  # Gets minimum day of flowering
-  slice(which.min(jday)) -> phen_flower
-
-## Kinship stuff ####
-
-# Read in data that matches kinship matrix position and genotype ID
-kinshipIDs <- read_csv("~/Git/Bromecast/gardens/rawdata/cg_psuDTF.csv")
-
-kinshipIDs %>% 
-  # Two genotypes are currently absent from the kinship matrix
-  filter(source != "Adler09" & source != "Shriver01") %>% 
-  arrange(kinshipID)-> genotypes_93
-
-# Read in kinship matrix
-setwd("~/Git/Bromecast/gardens/rawdata/")
-kinship93BRTE <- npyLoad("93BRTEcg.kinship.npy")
-setwd("~/Git/Bromecast-phenology/")
-
-# Put genotype numbers on rows and columns
-colnames(kinship93BRTE) <- as.factor(genotypes_93$genotype)
-rownames(kinship93BRTE) <- as.factor(genotypes_93$genotype)
-
-# Subset phen data for only genotypes that we have kinship data for
-phen_flower %>% 
-  filter(genotype %in% genotypes_93$genotype) %>% 
-  mutate(genotype = as.factor(genotype))-> phen_flower_sub
-# Right now this only drops 213 plants total
-
-# And vice versa for kinship matrix
-keeps <- which(rownames(kinship93BRTE) %in% unique(phen_flower_sub$genotype))
-
-kinship93BRTE[keeps, keeps] -> kin
-
-phen_flower %>% 
-  filter(genotype %in% rownames(kin)) -> phen_flower_kin
-
-
-## Fit linear model ####
-start <- Sys.time()
-
-gam_linear <- gam(jday ~ density*gravel*pc1 + density*gravel*pc2 +
-                    site*pc1 + site*pc2 +
-                    # Random intercept for block
-                    s(block_unique, bs = 're') +
-                    # Random intercept for plot nested within block
-                    s(plot_unique, bs = 're') +
-                    s(genotype, bs = 're') +
-                    s(genotype, density, bs = 're') +
-                    s(genotype, site, bs = 're') +
-                    s(genotype, gravel, bs = "re"), method = "REML",
-                  data = phen_flower)
-
-end <- Sys.time()
-
-library(lmerTest)
-
-gam_lmer <- lmer(jday ~ density*gravel*pc1 + density*gravel*pc2 +
-                         site*pc1 + site*pc2 +
-                         (1|block_unique) + (1|plot_unique) + (density + site + gravel | genotype),
-                       data = phen_flower)
-
-plot_model(gam_lmer, type = "emm", terms = c("pc2"))
+# Source in compiled data for the model
+source(here("supp_code", "compile_data.R"))
 
 # Fit Bayesian linear model 
 start <- Sys.time()
 
 brms_m1 <- brm(
-  jday ~ 1 + density * gravel * site +
+  jday ~ 1 + density * gravel * pc1 + density * gravel * pc2 +
     site * pc1 + site * pc2 + (1 + density + gravel + site || gr(genotype, cov = Amat)) +
     (1 | block_unique) + (1 | plot_unique),
   data = phen_flower_kin,
   data2 = list(Amat = kin),
   family = gaussian(),
-  chains = 3, cores = 1, iter = 5000
+  chains = 3, cores = 1, iter = 1000
 )
+
+end <- Sys.time()
+
+end - start
+
+summary(brms_m1)
+
+# Preliminary results graphs
+
+theme_set(theme_bw(base_size = 16))
+
+png("figs/prelim_int.png", height = 4, width = 8, res = 300, units = "in")
+sjPlot::plot_model(brms_m1, type = "emm", terms = c("pc2","density", "gravel")) +
+  scale_color_manual(values = c("orange", "dodgerblue")) + ggtitle("")
+dev.off()
+
+png("figs/prelim_site.png", height = 5, width = 5.5, res = 300, units = "in")
+sjPlot::plot_model(brms_m1, type = "emm", terms = c("gravel","density","site")) +
+  scale_color_manual(values = c("orange", "dodgerblue")) + ggtitle("")
+dev.off()
+  
+
+mod <- lmer(jday ~ density * gravel * pc1 + density * gravel * pc2 + site * pc1 + site * pc2 +
+              (1|block_unique) + (1|plot_unique) + (1+density+gravel+site|genotype), data = phen_flower_kin)
+
+anova(mod)
+sjPlot::plot_model(mod, type = "pred", pred.type = "re", terms = c("site", "genotype"), se = F) +
+  geom_line() + theme(legend.position = "none")
+
+ranef(mod)
+
+
+emmeans::emmeans(mod, ~site)
+
+mod2 <- lmer(jday ~ density * gravel + site * pc1 + site * pc2 +
+               (1|block_unique) + (1|plot_unique) + (1|genotype), data = phen_flower_kin)
+
+anova(mod, mod2)
+
+plot_model(mod, type = "pred", pred.type = "re", terms = c("density", "genotype"), se = F) +
+  geom_line()
 
 end <- Sys.time()
 
 # Time difference of 1.263403 hours
 
 # Save this first run as a model object
-saveRDS(brms_m1, "supp_data/brms_flower.rds")
+# saveRDS(brms_m1, "supp_data/brms_flower.rds")
+brms_m1 <- readRDS("supp_data/brms_flower.rds")
+
+
+
+newdata <- expand_grid(gravel = c("black", "white"),
+                       density = c("hi", "lo"),
+                       genotype = unique(phen_flower_kin$genotype),
+                       site = unique(phen_flower_kin$site),
+                       pc1 = mean(phen_flower_kin$pc1),
+                       pc2 = mean(phen_flower_kin$pc2),
+                       block_unique = "SS_1",
+                       plot_unique = "SS_4_2")
+
+predict(mod, newdata) -> preds
+
+newdata %>% 
+  group_by(density, genotype) %>% 
+  summarize(mean = mean(preds)) %>% 
+  ggplot(aes(x = density, y = mean, group = genotype)) +
+  geom_line()
+
+newdata$preds <- preds
+
+epred_draws(brms_m1, newdata = newdata, re_formula = NULL, allow_new_levels = T) %>% 
+  group_by(density, genotype) %>% 
+  summarize(mean = mean(.epred)) %>% 
+  spread(key = density, value = mean) %>% 
+  mutate(diff = hi - lo) %>% 
+  arrange(diff)
+  
+
+  
+
+  
+  ggplot(all_regions_autonomy_dist %>% filter(genotype %in% 1:10), 
+         aes(x = .epred, y = genotype, 
+             fill = site)) +
+    stat_halfeye() +
+    labs(x = "Predicted media index", y = NULL,
+         fill = "Opposition parties allowed",
+         subtitle = "Posterior predictions") +
+    theme(legend.position = "bottom")
 
 summary(brms_m1)
 
@@ -210,7 +123,48 @@ theme_set(theme_bw(base_size = 14))
 sjPlot::plot_model(brms_m1, type = "emm", terms = c("density", "gravel", "site"))
 sjPlot::plot_model(brms_m1, type = "emm", terms = c("pc1", "site"))
 sjPlot::plot_model(brms_m1, type = "emm", terms = c("pc2", "site"))
-sjPlot::plot_model(brms_m1, type = "slope")
+
+ranef(brms_m1)
+
+plot_model(brms_m1, type = "pred", 
+           terms = c("density", "r_genotype [1:3]"), 
+           pred.type = "re")
+
+fitted <- fitted(brms_m1)
+post_pred <- posterior_predict(brms_m1)
+lower <- apply(post_pred, quantile, probs = c(0.025), MARGIN = 2)
+upper <- apply(post_pred, quantile, probs = c(0.975), MARGIN = 2)
+
+phen_flower_kin %>% 
+  bind_cols(as_tibble(fitted))-> phen_fitted 
+  
+
+#group_by(genotype, density) %>% 
+  #summarize(mean = mean(Estimate)) %>% 
+phen_fitted %>% 
+  ggplot(aes(x = jday, y = Estimate)) + geom_point() +
+  geom_linerange(aes(ymin = lower, ymax = upper)) +
+  geom_abline(aes(intercept = 0, slope = 1))
+
+give_me_R2 <- function(preds,actual){
+  rss <- sum(( preds - actual ) ^ 2)  ## residual sum of squares
+  tss <- sum((actual - mean(actual)) ^ 2)  ## total sum of squares
+  rsq <- 1 - rss/tss
+  return(rsq)
+}
+
+random_persons <- sample(unique(phen_flower_kin$genotype), size = 20)
+conditional_effects(brms_m1, type = "pred", effects = "density", 
+                    re_formula = NULL, 
+                    conditions = tibble(genotype = c("64", "48"))
+)
+
+brms_m1 %>% 
+  emmeans(~density+genotype, re_formula = ~(1|genotype))
+
+give_me_R2(phen_fitted$Estimate, phen_fitted$jday)
+
+summary(brms_m1)
 
 # Calculate heritability
 v_animal <- (VarCorr(brms_m1, summary = FALSE)$genotype$sd[,1])^2
---
+++ supp_code/compile_data.R
@@ -0,0 +1,146 @@
+# This file reads in and compiles the data needed for the flower timing analysis
+
+## Load libraries ####
+library(tidyverse); library(mgcv); library(gratia); library(geomtextpath);
+library(here); library(readr); library(brms); library(RcppCNPy)
+
+## Source code for genotype climate of origin ####
+source(here("supp_code/climate_of_origin.R"))
+
+## Read in Sheep Station data ####
+
+# Read in derived phenology data
+phen_SS <- read_csv("~/Git/Bromecast/gardens/deriveddata/SS2022_growthphenology_with_harvest.csv")
+# Read in plant ID info
+ids_SS <- read_csv("~/Git/Bromecast/gardens/deriveddata/SS2022_plantID.csv")
+# Read in flagging data
+flags_SS <- read_csv("~/Git/Bromecast/gardens/deriveddata/SS2022_flags.csv")
+gardens <- read_csv("~/Git/Bromecast/gardens/rawdata/garden_treatments.csv")
+# Merge together datasets
+phen_id_SS <- merge(phen_SS, ids_SS)
+
+# Rename 'garden' column and remove cum_plot column
+gardens %>% 
+  mutate(site = garden) %>% 
+  dplyr::select(-cum_plot, -garden) -> gardens_sub
+
+# Merge together datasets
+phen_id_garden_SS <- merge(phen_id_SS, gardens_sub)
+
+# Merge together datasets
+phen_SS <- merge(phen_id_garden_SS, flags_SS)
+phen_SS <- merge(phen_SS, genotype_PCclimate)
+
+# Set appropriate factors for variables
+phen_SS %>% 
+  mutate(block = as.factor(block),
+         plot = as.factor(plot),
+         growout = as.factor(growout),
+         density = as.factor(density),
+         gravel = as.factor(gravel),
+         site = as.factor(site),
+         genotype = as.factor(genotype)) %>% 
+  mutate(plot_unique = as.factor(paste(site, block, plot, sep = "_")),
+         block_unique = as.factor(paste(site, block, sep = "_")))-> phen_SS
+
+
+## Read in Boise data ####
+# Read in derived phenology data
+phen_Boise <- read_csv("~/Git/Bromecast/gardens/deriveddata/Boise2022_growthphenology_by_plantID.csv")
+# Read in plant ID info
+ids_Boise <- read_csv("~/Git/Bromecast/gardens/deriveddata/Boise2022_plantID.csv")
+# Read in flagging data
+flags_Boise <- read_csv("~/Git/Bromecast/gardens/deriveddata/Boise2022_flags.csv")
+
+# Merge together datasets
+phen_id_Boise <- merge(phen_Boise, ids_Boise)
+
+# Merge together datasets
+phen_id_garden_Boise <- merge(phen_id_Boise, gardens_sub)
+
+# Merge together datasets
+phen_Boise <- merge(phen_id_garden_Boise, flags_Boise)
+phen_Boise <- merge(phen_Boise, genotype_PCclimate)
+
+# Set appropriate factors for variables
+phen_Boise %>% 
+  mutate(block = as.factor(block),
+         plot = as.factor(plot),
+         growout = NA,
+         density = as.factor(density),
+         gravel = as.factor(gravel),
+         site = as.factor(site),
+         genotype = as.factor(genotype)) %>% 
+  mutate(plot_unique = as.factor(paste(site, block, plot, sep = "_")),
+         block_unique = as.factor(paste(site, block, sep = "_")))-> phen_Boise
+
+
+## Read in Cheyenne data ####
+# Read in derived phenology data
+phen_CH <- read_csv("~/Git/Bromecast/gardens/deriveddata/CH2022_growthphenology_by_plantID.csv")
+# Read in plant ID info
+ids_CH <- read_csv("~/Git/Bromecast/gardens/deriveddata/CH2022_plantID.csv")
+# Read in flagging data
+flags_CH <- read_csv("~/Git/Bromecast/gardens/deriveddata/CH2022_flags.csv")
+
+# Merge together datasets
+phen_id_CH <- merge(phen_CH, ids_CH)
+
+# Merge together datasets
+phen_CH <- merge(phen_id_CH, flags_CH)
+phen_CH <- merge(phen_CH, genotype_PCclimate)
+
+# Set appropriate factors for variables
+phen_CH %>% 
+  mutate(block = as.factor(block),
+         plot = as.factor(plot),
+         growout = NA,
+         density = as.factor(case_when(density == "high" ~ "hi",
+                                       density == "low" ~ "lo")),
+         gravel = as.factor(gravel),
+         site = as.factor(site),
+         genotype = as.factor(genotype)) %>% 
+  mutate(plot_unique = as.factor(paste(site, block, plot, sep = "_")),
+         block_unique = as.factor(paste(site, block, sep = "_")))-> phen_CH
+
+## Merge datasets together ####
+phen <- rbind(phen_SS %>% dplyr::select(-tillers), phen_Boise, phen_CH)
+## Create plant level dataset for flowering time ####
+phen %>%
+  filter(v %in% c("FG", "FP", "FB", "FX")) %>%
+  group_by(plantID) %>%
+  # Gets minimum day of flowering
+  slice(which.min(jday)) -> phen_flower
+## Read in kinship data and subset flowering data to match ####
+## Kinship stuff ####
+
+# Read in data that matches kinship matrix position and genotype ID
+kinshipIDs <- read_csv("~/Git/Bromecast/gardens/rawdata/cg_psuDTF.csv")
+
+kinshipIDs %>% 
+  # Two genotypes are currently absent from the kinship matrix
+  filter(source != "Adler09" & source != "Shriver01") %>% 
+  arrange(kinshipID)-> genotypes_93
+
+# Read in kinship matrix
+setwd("~/Git/Bromecast/gardens/rawdata/")
+kinship93BRTE <- npyLoad("93BRTEcg.kinship.npy")
+setwd("~/Git/Bromecast-phenology/")
+
+# Put genotype numbers on rows and columns
+colnames(kinship93BRTE) <- as.factor(genotypes_93$genotype)
+rownames(kinship93BRTE) <- as.factor(genotypes_93$genotype)
+
+# Subset phen data for only genotypes that we have kinship data for
+phen_flower %>% 
+  filter(genotype %in% genotypes_93$genotype) %>% 
+  mutate(genotype = as.factor(genotype))-> phen_flower_kin
+# Right now this only drops 213 plants total
+
+# And vice versa for kinship matrix
+keeps <- which(rownames(kinship93BRTE) %in% unique(phen_flower_kin$genotype))
+
+kinship93BRTE[keeps, keeps] -> kin
+
+# Remove all other intermediate data sets
+rm(list=setdiff(ls(), c("phen_flower_kin", "kin")))
